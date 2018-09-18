5a35b5f570724e52d891b5b9802dabc5885280fc
Nikolay <nvovk@bigbangbutton.com>
2018-09-18 12:18:31 +0300

    Finish theme

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,6 @@
 export default {
   color: {
+    // b2c
     purple: '#7700ff',
     orange: '#ff4f11',
     darkBlue: '#273a64',
@@ -8,5 +9,24 @@ export default {
     lightBlue: '#65b8df',
     pink: '#ffbfc3',
     green: '#57d987',
+    // monochrome
+    black100: 'rgba(16,24,40,1)',
+    black90: 'rgba(16,24,40,.9)',
+    black80: 'rgba(16,24,40,.8)',
+    black70: 'rgba(16,24,40,.7)',
+    black60: 'rgba(16,24,40,.6)',
+    grey: '#979799',
+    coolGray: '#e3e8ec',
+    coolGray50: 'rgba(227,233,236,.5)',
+  },
+  size: {
+    s: 12,
+    m: 16,
+    l: 20,
+  },
+  weight: {
+    medium: '500',
+    regular: '400',
+    light: '300',
   },
 }
