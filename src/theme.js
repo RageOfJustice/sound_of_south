7dd811ca99950312e546f2b9677a54ac7a95bd0c
Nikolay <nvovk@bigbangbutton.com>
2018-09-18 12:08:03 +0300

    Add b2c colors

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,12 @@
 export default {
-  color: {},
+  color: {
+    purple: '#7700ff',
+    orange: '#ff4f11',
+    darkBlue: '#273a64',
+    red: '#bc104b',
+    yellow: '#ffbe08',
+    lightBlue: '#65b8df',
+    pink: '#ffbfc3',
+    green: '#57d987',
+  },
 }
