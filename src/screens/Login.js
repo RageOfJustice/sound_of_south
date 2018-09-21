810f106d71e96d40335a34b0f2bc22bc11990196
Nikolay <nvovk@bigbangbutton.com>
2018-09-21 20:47:38 +0300

    Add description to ligin screen

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -3,6 +3,22 @@ import React from 'react'
 import styled from 'styled-components'
 import type { navigationParams } from '../utils'
 import { LoginFormContainer as LoginForm } from '../containers'
+import {Text} from 'react-native-elements'
+
+
+const DescriptionText = styled(Text)`
+  color: ${({theme}) => theme.color.black100};
+  font-size: ${({theme}) => theme.size.m};
+  text-align: center;
+`
+
+const OrangeText = DescriptionText.extend`
+  color:  ${({theme}) => theme.color.orange};
+`
+
+const DescriptionWrapper = styled.View`
+  padding-vertical: 20px;
+`
 
 const Container = styled.View`
   flex: 1;
@@ -19,6 +35,17 @@ class Login extends React.Component<Props> {
     return (
       <Container>
         <LoginForm logoText="Голос Юга" />
+        <DescriptionWrapper>
+        <DescriptionText>
+          Для входа введите свои данные в формате:
+        </DescriptionText>
+        <OrangeText>
+          ivan.ivanov
+        </OrangeText>
+        <OrangeText>
+          пароль в корп. системе
+        </OrangeText>
+        </DescriptionWrapper>
       </Container>
     )
   }
